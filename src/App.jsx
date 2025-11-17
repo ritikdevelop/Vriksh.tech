373ac35b0e678601d8a221b6fab188d60e56c1aa
ritikdevelop <hrithikrana216@gmail.com>
2025-11-17 13:27:06 +0530

    `Updated icons and links in various components`

--- src/App.jsx
+++ src/App.jsx
@@ -8,14 +8,19 @@ import Home from "./pages/Home";
 import About from "./pages/About";
 import Footer from "./components/Footer";
 import Services from "./pages/Services";
+import UiUxDesign from "./pages/UiUxDesign";
+import AppDevelopment from "./pages/AppDevelopment";
+import ProductDesign from "./pages/ProductDesign";
+import WebsiteDesign from "./pages/WebsiteDesign";
+import BusinessAnalysis from "./pages/BusinessAnalysis";
+import WebDevelopment from "./pages/WebDevelopment";
 import Blog from "./pages/Blog";
 import Contact from "./pages/Contact";
-
+import DigitalMarketing from "./pages/DigitalMarketing";
 
 const App = () => {
   return (
     <div>
-      
       <CustomCursor />
       {/* <PreLoader /> */}
       <Header />
@@ -23,12 +28,24 @@ const App = () => {
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/services" element={<Services />} />
+        <Route path="/services/ui-ux-design" element={<UiUxDesign />} />
+        <Route path="/services/app-development" element={<AppDevelopment />} />
+        <Route path="/services/product-design" element={<ProductDesign />} />
+        <Route path="/services/website-design" element={<WebsiteDesign />} />
+        <Route
+          path="/services/business-analysis"
+          element={<BusinessAnalysis />}
+        />
+        <Route path="/services/web-development" element={<WebDevelopment />} />
+        <Route
+          path="/services/digital-marketing"
+          element={<DigitalMarketing />}
+        />
         <Route path="/blog" element={<Blog />} />
         <Route path="/contact" element={<Contact />} />
       </Routes>
       <Chat />
       <Footer />
-
     </div>
   );
 };
