26db3ca2d7879e709b3fb051fc1dc9474a382efe
ritikdevelop <hrithikrana216@gmail.com>
2025-11-24 10:54:38 +0530

    Refactored CustomCursor component to improve hover detection and updated CSS for consistency

--- src/components/CustomCursor.jsx
+++ src/components/CustomCursor.jsx
@@ -3,16 +3,33 @@ import "./CustomCursor.css";
 
 const CustomCursor = () => {
   const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
+  const [isHovering, setIsHovering] = useState(false);
 
   useEffect(() => {
     const handleMouseMove = (e) => {
       setMousePosition({ x: e.clientX, y: e.clientY });
     };
 
+    const handleMouseOver = (e) => {
+      if (e.target.closest("a, button, .hover-target")) {
+        setIsHovering(true);
+      }
+    };
+
+    const handleMouseOut = (e) => {
+      if (e.target.closest("a, button, .hover-target")) {
+        setIsHovering(false);
+      }
+    };
+
     window.addEventListener("mousemove", handleMouseMove);
+    window.addEventListener("mouseover", handleMouseOver);
+    window.addEventListener("mouseout", handleMouseOut);
 
     return () => {
       window.removeEventListener("mousemove", handleMouseMove);
+      window.removeEventListener("mouseover", handleMouseOver);
+      window.removeEventListener("mouseout", handleMouseOut);
     };
   }, []);
 
@@ -26,7 +43,7 @@ const CustomCursor = () => {
         }}
       ></div>
       <div
-        className="custom-cursor__cursor-two"
+        className={`custom-cursor__cursor-two ${isHovering ? "custom-cursor__innerhover" : ""}`}
         style={{
           left: `${mousePosition.x}px`,
           top: `${mousePosition.y}px`,
